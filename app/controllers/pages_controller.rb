cbdff61c4f957b158755a4aebb95df8d7f5071b8
tiago landi <tiagolandi@gmail.com>
2020-09-30 11:06:16 -0300

    pagina grids e cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :grid, :card]
 
   def home
   end
+
+  def grid
+  end
+
+  def card
+  end
+
 end
