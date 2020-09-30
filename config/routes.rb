49ad4e88ffc075238b96966c1c4ca925f735c536
tiago landi <tiagolandi@gmail.com>
2020-09-30 11:45:02 -0300

    adding more pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "grid", to: "pages#grid"
   get "card", to: "pages#card"
+  get "button", to: "pages#button"
 end
